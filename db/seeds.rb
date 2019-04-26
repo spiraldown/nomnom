a348f1855c4c504ab78ee4993ebfead2b92172a7
Neen <47649528+spiraldown@users.noreply.github.com>
2019-04-26 22:41:03 +0000

    added something to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name:"Del Taco 1",address:"123 Arapahoe Rd, Centennial, CO 80015",description:"Best Chili Cheese Fries in Colorado!")
+# Place.create(name:"Rumbi\'s",740 W Telegraph St, St.George, UT 84780,The on Rumbi\'s on our way to California! Good Rice bowls!"
+# Place.create(name:"Rounter Table Pizza",1010 Riverside Dr, Chino, CA 91710,Amazing PIzza. Though really expensive."
+# Place.create(name:"Pizza Pirates",4256 Mountain Ave, Chino, CA 91710,Most amazing pizza. On par easily with Round table and costs far less!"
+# "In-N-Out,2394 Mountain Ave, Chino, CA 91710,Most amazing burgers! Awesome!"
+# "Tommy's Burger's,9849 Mountain Ave, Chino, CA 91710,Incredible Chili Cheese Fries. Yum!"
+# "Yum Yum Donut's,1294 Riverside Drive, Chino, CA 91710,Great donuts. Nice Firtters"
+# "Little Ceaser's Pizza,93273 BBuckley, Aurora, CA 80015, Hot and ready! Tasty Deep Dish!"
+# "Five Guy's,9384 Smokey Hill Rd, Centennial, CO 80015,Fun place to eat. Peanuts and burgers."
+# "Subway,3928 Unit B, Centennial, CO 80015,Eat fresh!"
+# "Celebrity Lane's,2392 Arapahoe Road, Centennial, CO 80015,Bowling and games. Fun place and good times."
+# "Golden Corral,5382 S. Parker Rd, Centennial, CO 80015,All you can eat food. Expensive but not bad"
+# "Red Robin,4758 Arapahoe Rd, Greenwood Village, CO 80111, Great gourmet burgers"
+# "Burger King,9756 Smokey Hill, Centennial, CO 80015,Value meal and coupons. Really good if you get money off"
+# "Chik Filet,94939 Parker Road, Centennial, CO 80014,Wooo waffle fries!"
+# "Winchell's Donuts,3938 Chamers, Aurora, CO 80015, Awesome donuts in colorado"
+# "Imagarifix,2405 Smokey Hill Rd, Centennial,CO 80015,Amazing owner. Great Ice cream! fun place!"
+# "Cafe China,3927 Main St., Parker, CO 82013,Yummy Chinese Food"
+# "Cafe Ambrosia,8493 Smokey Hill Rd., Centennial, CO 80015,Amazing Lemon Chicken"
+# "Cafe Ambrosia,8493 Smokey Hill Rd., Centennial, CO 80015,Amazing Lemon Chicken"
+# "Some Random place!,4232 Random Drive, Rando, CO 80015,Totally random food!"
+# "Monster Golf,2394 Somewhere Greenwood Village, CO 80015,Pretty nice place to golf for a coupon price. "

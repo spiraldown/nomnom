15bdc3504dce73766b82bcc7b3b46454e6009818
Neen <47649528+spiraldown@users.noreply.github.com>
2019-04-29 07:23:20 +0000

    added Edit user window and features

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,6 +16,11 @@ before_action :authenticate_user!, only: [:new, :create]
 def show
   @place = Place.find(params[:id])
 end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
   private
 
   def place_params

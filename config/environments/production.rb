24a56acc83168435ebfc15a054c1464ba0c11b87
Neen <47649528+spiraldown@users.noreply.github.com>
2019-05-11 02:01:33 +0000

    added email for heorku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
   config.action_mailer.default_url_options = { host: 'nomster-matt-munos.herokuapp.com' }
 
   # Code is not reloaded between requests.
